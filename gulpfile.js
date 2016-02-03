0a995acc8305d87d8e7cff06803f23d74d236a45
tvikulov <tvikulov@usetech.ru>
2016-02-03 12:54:17 +0300

    project compleate

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@
 
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
     uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     rigger = require('gulp-rigger'),
     cssmin = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
     rimraf = require('rimraf'),
     browserSync = require("browser-sync"),
     reload = browserSync.reload;
@@ -54,7 +57,7 @@ gulp.task('html:build', function () {
 });
 
 gulp.task('js:build', function () {
-     gulp.src(path.src.js) //Найдем наш main файл
+    gulp.src(path.src.js) //Найдем наш main файл
         .pipe(rigger()) //Прогоним через rigger
         .pipe(sourcemaps.init()) //Инициализируем sourcemap
         .pipe(uglify()) //Сожмем наш js
@@ -63,25 +66,36 @@ gulp.task('js:build', function () {
         .pipe(reload({stream: true})); //И перезагрузим сервер
 });
 
-gulp.task('image:build', function () {
-    gulp.src(path.src.img)
-        .pipe(gulp.dest(path.build.img))
-});
-
-gulp.task('fonts:build', function() {
-    gulp.src(path.src.fonts)
-        .pipe(gulp.dest(path.build.fonts))
-});
-
 gulp.task('style:build', function () {
-    gulp.src(path.src.style) //Выберем наш styles.scss
+    gulp.src(path.src.style) //Выберем наш main.scss
         .pipe(sourcemaps.init()) //То же самое что и с js
         .pipe(sass()) //Скомпилируем
-        //.pipe(prefixer()) //Добавим вендорные префиксы
+        .pipe(prefixer()) //Добавим вендорные префиксы
         .pipe(cssmin()) //Сожмем
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(path.build.css)) //И в build
         .pipe(reload({stream: true}));
+});
+
+gulp.task('image:build', function () {
+    gulp.src(path.src.img) //Выберем наши картинки
+        .pipe(imagemin({ //Сожмем их
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img)) //И бросим в build
+        .pipe(reload({stream: true}));
+});
+/*gulp.task('image:build', function () {
+    gulp.src(path.src.img)
+        .pipe(gulp.dest(path.build.img))
+});*/
+
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
 });
 
 
--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,9 @@
  * Third party
  */
 //= ../../bower_components/jquery/dist/jquery.js
-//= ../../bower_components/jquery-validation/build/release.js
 
 
 /*
  * Custom
  */
 //= modules/actions.js
-//= modules/validate.js
